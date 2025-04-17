eabfb9a12b3c31d5628a37f4994de84f76bc8a96
mdirtezasaquib <mdsaquib140802@gmail.com>
2025-04-17 12:55:30 +0530

    Update JobController.java

--- src/main/java/in/codingage/jobportal/controller/JobController.java
+++ src/main/java/in/codingage/jobportal/controller/JobController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/Job")
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "https://jobportalbackend-cd60.onrender.com")
 public class JobController {
 
     @Autowired
